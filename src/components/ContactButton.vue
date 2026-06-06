15d5f6b9789909b9f0dea001a2ed446a72631a93
Yem Daro <daroyem99@gmail.com>
2026-06-06 21:55:01 +0700

    add contacts in profile section

---
+++ src/components/ContactButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  url?: string
+  imgSrc?: string
+  alt?: string
+}>()
+</script>
+
+<template>
+  <a
+    :href="url"
+    target="_noblank"
+    rel="noopener noreferrer"
+  >
+    <img
+      :src="imgSrc" :alt="alt" width="40"
+      loading="lazy"
+      class="rounded-lg"
+      :class="$attrs.class"
+    >
+  </a>
+</template>
--- src/components/ProfileSection.vue
+++ src/components/ProfileSection.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import ContactButton from './ContactButton.vue'
 
+const baseUrl = import.meta.env.BASE_URL
 </script>
 
 <template>
@@ -11,6 +13,29 @@
       <h2 class="text-3xl">
         Fullstack Developer
       </h2>
+    </div>
+    <div class="flex items-center justify-center gap-4 mt-4">
+      <ContactButton
+        url="https://github.com/darororo"
+        :img-src="`${baseUrl}/github.png`"
+        alt="github"
+      />
+      <ContactButton
+        url="https://t.me/daroyem"
+        :img-src="`${baseUrl}/telegram.webp`"
+        alt="telegram"
+      />
+      <ContactButton
+        url="mailto:daroyem99@gmail.com"
+        :img-src="`${baseUrl}/gmail.svg`"
+        alt="gmail"
+        class="bg-white"
+      />
+      <ContactButton
+        url="https://www.linkedin.com/in/daro-yem-723b66229/"
+        :img-src="`${baseUrl}/linkedin.webp`"
+        alt="linkedin"
+      />
     </div>
   </div>
 </template>
